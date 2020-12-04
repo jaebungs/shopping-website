a9602ae667dd399353937f7964853c8df5f398d1
jaebungs <jaebungs@gmail.com>
2020-12-03 20:06:18 -0500

    update styles for NotFoundPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-    <div>
-    <h1>404 Not Found</h1>
+    <div className="notFoundPage-container">
+        <h1>404 Not Found</h1>
         <Link to='/'>Go to Home</Link>
     </div>
 );
