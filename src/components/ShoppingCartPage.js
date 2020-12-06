c8cc9fbbd414cba260181eb197f527d2c7738e8b
jaebungs <jaebungs@gmail.com>
2020-12-06 00:11:16 -0500

    updat ShoppingCartPage grid styles

--- src/components/ShoppingCartPage.js
+++ src/components/ShoppingCartPage.js
@@ -4,17 +4,24 @@ import ShoppingCartProductsTemplate from './ShoppingCartProducts';
 
 const ShoppingCartPage = (props) => {
     return (
-        <div>
-            <h1>{props.carts.length}</h1>
-            {
-                props.carts.map((cart, index) => {
-                    return <ShoppingCartProductsTemplate 
-                                key={cart.id}
-                                index={index}
-                                {...cart} 
-                            />
-                })
-            }
+        <div className="shoppingCartPage-container">
+            <div className="cart-subtitle-container">
+                <h3>No.</h3>
+                <h3>Pic</h3>
+                <h3>Product</h3>
+                <h3>Quantity</h3>
+                <h3>Price</h3>
+                <h3>Btn</h3>
+            </div>
+                {
+                    props.carts.map((cart, index) => {
+                        return <ShoppingCartProductsTemplate 
+                                    key={cart.id}
+                                    index={index}
+                                    {...cart} 
+                                />
+                    })
+                }
         </div>
     )
 }
--- src/components/ShoppingCartProducts.js
+++ src/components/ShoppingCartProducts.js
@@ -3,10 +3,19 @@ import React from 'react';
 const ShoppingCartProductsTemplate = ({ id, img, name, price, quantity, index }) => {
     return (
         <div className="cart-product-card">
-            <p>{index}</p>
+            <h4>{index+1}.</h4>
             <img src={img}/>
             <p>{name}</p>
-            <p>{price}</p>
+            <div>
+                <button className="quantity-btn btn">-</button>
+                <label htmlFor={"quantityFor"+id}>
+                    <input type="text" name="quantity" id={"quantityFor"+ id} value={quantity} readOnly />
+                </label>
+                <button className="quantity-btn btn">+</button>
+            </div>
+
+            <p>{price * quantity}$ CAD</p>
+            <button className="btn">Remove</button>
         </div>
     )
 }
